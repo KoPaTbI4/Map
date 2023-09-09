38f971dcac28c1651fa7f50df420bcf2b49b778a
KoPaTbI4 <bormotov1998@gmail.com>
2023-09-10 05:54:27 +0700

    Add

---
+++ Map/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Map
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._gMapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.b_addMarker = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.TextBox();
+            this.Coord_Y = new System.Windows.Forms.TextBox();
+            this.Coord_X = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _gMapControl
+            // 
+            this._gMapControl.Bearing = 0F;
+            this._gMapControl.CanDragMap = true;
+            this._gMapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this._gMapControl.GrayScaleMode = false;
+            this._gMapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this._gMapControl.LevelsKeepInMemory = 5;
+            this._gMapControl.Location = new System.Drawing.Point(0, 0);
+            this._gMapControl.MarkersEnabled = true;
+            this._gMapControl.MaxZoom = 2;
+            this._gMapControl.MinZoom = 2;
+            this._gMapControl.MouseWheelZoomEnabled = true;
+            this._gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this._gMapControl.Name = "_gMapControl";
+            this._gMapControl.NegativeMode = false;
+            this._gMapControl.PolygonsEnabled = true;
+            this._gMapControl.RetryLoadTile = 0;
+            this._gMapControl.RoutesEnabled = true;
+            this._gMapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this._gMapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this._gMapControl.ShowTileGridLines = false;
+            this._gMapControl.Size = new System.Drawing.Size(624, 448);
+            this._gMapControl.TabIndex = 0;
+            this._gMapControl.Zoom = 0D;
+            // 
+            // b_addMarker
+            // 
+            this.b_addMarker.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.b_addMarker.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_addMarker.Location = new System.Drawing.Point(39, 194);
+            this.b_addMarker.Name = "b_addMarker";
+            this.b_addMarker.Size = new System.Drawing.Size(93, 30);
+            this.b_addMarker.TabIndex = 1;
+            this.b_addMarker.Text = "Добавить";
+            this.b_addMarker.UseVisualStyleBackColor = true;
+            this.b_addMarker.Click += new System.EventHandler(this.b_addMarker_Click);
+            // 
+            // Title
+            // 
+            this.Title.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Title.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(32, 151);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(108, 25);
+            this.Title.TabIndex = 2;
+            this.Title.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Coord_Y
+            // 
+            this.Coord_Y.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Coord_Y.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Coord_Y.Location = new System.Drawing.Point(32, 95);
+            this.Coord_Y.Name = "Coord_Y";
+            this.Coord_Y.Size = new System.Drawing.Size(108, 25);
+            this.Coord_Y.TabIndex = 3;
+            this.Coord_Y.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Coord_X
+            // 
+            this.Coord_X.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Coord_X.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Coord_X.Location = new System.Drawing.Point(31, 39);
+            this.Coord_X.Name = "Coord_X";
+            this.Coord_X.Size = new System.Drawing.Size(108, 25);
+            this.Coord_X.TabIndex = 4;
+            this.Coord_X.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Coord_X_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Координата Х";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Координата Y";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Имя";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(656, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(656, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(565, 448);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.b_addMarker);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Coord_Y);
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Controls.Add(this.Coord_X);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(654, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 248);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this._gMapControl);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Map";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private GMap.NET.WindowsForms.GMapControl _gMapControl;
+        private System.Windows.Forms.Button b_addMarker;
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.TextBox Coord_Y;
+        private System.Windows.Forms.TextBox Coord_X;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Map/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace Map
+{ 
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            _gMapControl.Dock = DockStyle.Fill;
+            _gMapControl.MouseUp += _gMapControl_MouseUp;
+            _gMapControl.MouseDown += _gMapControl_MouseDown;
+            _gMapControl.MouseMove += _gMapControl_MouseMove;
+        }
+
+        private GMapMarker _selectedMarker;
+
+        public struct Markers 
+        {
+            public int id;
+            public double x;
+            public double y;
+            public string title;
+            public string type;
+        }
+
+        List<Markers> _markers = new List<Markers>();
+
+        int load_markers()
+        {
+            try
+            {
+                string connectionString = @"Data Source=NIKOLAY;Initial Catalog=MarkersDB;Integrated Security=True";
+                string query = "SELECT [ID],[Coordinates],[Title] FROM[dbo].[Markers]";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Markers _marker = new Markers();
+
+                        _marker.id = reader.GetInt32(0);
+
+                        string[] coord = reader.GetString(1).Split(',');
+
+                        _marker.x = Convert.ToDouble(coord[0].Replace(".", ","));
+                        _marker.y = Convert.ToDouble(coord[1].Replace(".", ","));
+                        _marker.title = reader.GetString(2);
+                        _marker.type = "old";
+
+                        _markers.Add(_marker);
+
+                        AddMarker(_marker.x, _marker.y, _marker.title);
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            { 
+                return -1;
+            }
+            return 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int ifail = load_markers();
+
+            if(ifail == -1)
+            {
+                MessageBox.Show("Ошибка работы с базой данных!", "Ошибка", MessageBoxButtons.OK);
+                this.Close();
+            }
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+           
+            _gMapControl.MapProvider = GMapProviders.GoogleMap;
+            _gMapControl.Position = new PointLatLng(_markers[0].x, _markers[0].y); 
+            _gMapControl.MinZoom = 1;
+            _gMapControl.MaxZoom = 18;
+            _gMapControl.Zoom = 12;
+            _gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            _gMapControl.CanDragMap = true;
+            _gMapControl.DragButton = MouseButtons.Left;
+            _gMapControl.ShowCenter = false;
+            _gMapControl.ShowTileGridLines = false;
+
+            
+        }
+
+        private void AddMarker(double lat, double lng, string title)
+        {
+            GMapMarker _marker = new GMarkerGoogle(new PointLatLng(lat, lng), GMarkerGoogleType.red);
+            _marker.ToolTipText = "\n" + title;
+            GMapOverlay markersOverlay = new GMapOverlay("Markers");
+            markersOverlay.Markers.Add(_marker);
+            _gMapControl.Overlays.Add(markersOverlay);
+
+            _gMapControl.Update();
+        }
+
+        bool checkTitle(string newtitle)
+        {
+            foreach (Markers _marker in _markers)
+            {
+                if (_marker.title == newtitle)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void b_addMarker_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Coord_X.Text) && string.IsNullOrEmpty(Coord_Y.Text) && string.IsNullOrEmpty(Title.Text))
+            {
+                MessageBox.Show("Необходимые поля не заполнены!");
+            }
+            else if (checkTitle(Title.Text))
+            {
+                MessageBox.Show("Введенное имя метки уже занято!");
+            }
+            else
+            {
+                Coord_X.Text = Coord_X.Text.Replace(".", ",");
+                Coord_Y.Text = Coord_Y.Text.Replace(".", ",");
+
+                Markers _marker = new Markers();
+
+                _marker.x = Convert.ToDouble(Coord_X.Text);
+                _marker.y = Convert.ToDouble(Coord_Y.Text);
+                _marker.title = Title.Text;
+                _marker.type = "new";
+
+                _markers.Add(_marker);
+
+                AddMarker(Convert.ToDouble(Coord_X.Text), Convert.ToDouble(Coord_Y.Text), Title.Text);
+
+                insertDB();
+            }
+        }
+
+        void updateDB()
+        {
+            string connectionString = @"Data Source=NIKOLAY;Initial Catalog=MarkersDB;Integrated Security=True";
+            string query = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                for (int i = 0; i < _markers.Count(); i++)
+                {
+                    if (_markers[i].type == "update")
+                    {
+                        Markers _marker = _markers[i];
+                        query = "UPDATE [dbo].[Markers] SET ";
+                        query += "[Coordinates] = '" + Convert.ToString(_marker.x).Replace(",", ".") + "," + Convert.ToString(_marker.y).Replace(",", ".") + "'";
+                        query += " Where ID = " + _marker.id;
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.ExecuteNonQuery();
+                        _marker.type = "old";
+                        _markers[i] = _marker;
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        void insertDB()
+        {
+            string connectionString = @"Data Source=NIKOLAY;Initial Catalog=MarkersDB;Integrated Security=True";
+            string query = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                for (int i =0; i < _markers.Count(); i++)
+                {
+                    if (_markers[i].type == "new")
+                    {
+                        Markers _marker = _markers[i];
+                        query = "INSERT INTO [dbo].[Markers] ([Coordinates],[Title]) VALUES (";
+                        query += "'" + Convert.ToString(_marker.x).Replace(",", ".") + "," + Convert.ToString(_marker.y).Replace(",", ".");
+                        query += "', '" + _marker.title + "') select SCOPE_IDENTITY()";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        _marker.id = Convert.ToInt32(command.ExecuteScalar());
+                        _marker.type = "old";
+                        _markers[i] = _marker;
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        void updateCoord(string title, double newX, double newY)
+        {
+            int id_marker = -1;
+            for (int i = 0; i < _markers.Count(); i++)
+            {
+                if (_markers[i].title == title.Substring(1))
+                {
+                    id_marker = i;
+                    break;
+                }
+            }
+            if (id_marker != -1)
+            {
+                Markers _marker = _markers[id_marker];
+                _marker.x = newX;
+                _marker.y = newY;
+                _marker.type = "update";
+                _markers[id_marker] = _marker;
+            }
+        }
+
+        private void _gMapControl_MouseDown(object sender, MouseEventArgs e)
+        {   
+            _selectedMarker = _gMapControl.Overlays.SelectMany(o => o.Markers).FirstOrDefault(m => m.IsMouseOver == true);
+        }
+
+        private void _gMapControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_selectedMarker is null)
+                return;
+
+            var latlng = _gMapControl.FromLocalToLatLng(e.X, e.Y); 
+            _selectedMarker.Position = latlng;
+            updateCoord(_selectedMarker.ToolTipText, latlng.Lat, latlng.Lng);
+            updateDB();
+            _selectedMarker = null;
+        }
+
+        private void _gMapControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_selectedMarker is null)
+                return;
+
+            var latlng = _gMapControl.FromLocalToLatLng(e.X, e.Y);
+            _selectedMarker.Position = latlng;
+            
+        }
+
+        private void Coord_X_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44 && number != 45 && number != 46) //цифры, клавиша BackSpace, точка и запятая а ASCII
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44 && number != 45 && number != 46) //цифры, клавиша BackSpace, точка и запятая а ASCII
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar <= 64 || e.KeyChar >= 91) && (e.KeyChar <= 96 || e.KeyChar >= 123) && number != 8 && number != 40 && number != 41 && number != 45 && number != 95) 
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
